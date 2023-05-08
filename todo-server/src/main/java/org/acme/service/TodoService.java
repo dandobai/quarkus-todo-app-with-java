5caaebb1f92df4739c6b10136289ec98d392e00f
dandobai <dandobai@gmail.com>
2023-05-08 19:17:47 +0200

    refactor todoService and implementation

--- todo-server/src/main/java/org/acme/service/TodoService.java
+++ todo-server/src/main/java/org/acme/service/TodoService.java
@@ -1,4 +1,18 @@
 package org.acme.service;
 
+import jakarta.ws.rs.core.Response;
+import org.acme.model.Todo;
+
+import java.util.List;
+
 public interface TodoService {
+    List<Todo> getTodos();
+
+    Todo getTodoById(Integer id);
+
+    Response createTodo(Todo todo);
+
+    Response updateTodo(Todo todo,Integer id);
+
+    Response deleteTodo(Integer id);
 }
--- todo-server/src/main/java/org/acme/service/TodoServiceImp.java
+++ todo-server/src/main/java/org/acme/service/TodoServiceImp.java
@@ -1,10 +1,69 @@
 package org.acme.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import org.acme.model.Todo;
 
+import java.util.List;
+
+@ApplicationScoped
 public class TodoServiceImp implements TodoService {
 
     @Inject
     EntityManager entityManager;
+
+    @Override
+    public List<Todo> getTodos() {
+        return entityManager.createNamedQuery("Todo.findAll", Todo.class)
+                .getResultList();
+    }
+
+    @Override
+    public Todo getTodoById(Integer id) {
+        Todo entity = entityManager.find(Todo.class, id);
+        if (entity == null) {
+            throw new WebApplicationException("Todo with id of " + id + " does not exist.", 404);
+        }
+        return entity;
+    }
+
+    @Override
+    public Response createTodo(Todo todo) {
+        if (todo.getId() != null) {
+            throw new WebApplicationException("Id was invalidly set on request.", 422);
+        }
+
+        entityManager.persist(todo);
+        return Response.ok(todo).status(201).build();
+    }
+
+    @Override
+    public Response updateTodo(Todo todo, Integer id) {
+        if (todo.getName() == null) {
+            throw new WebApplicationException("Fruit Name was not set on request.", 422);
+        }
+
+        Todo entity = entityManager.find(Todo.class, id);
+
+        if (entity == null) {
+            throw new WebApplicationException("Fruit with id of " + id + " does not exist.", 404);
+        }
+
+        entity.setName(todo.getName());
+
+        return Response.ok(entity).status(201).build();
+    }
+
+    @Override
+    public Response deleteTodo(Integer id) {
+        Todo entity = entityManager.getReference(Todo.class, id);
+        if (entity == null) {
+            throw new WebApplicationException("Todo with id of " + id + " does not exist.", 404);
+        }
+        entityManager.remove(entity);
+        return Response.status(204).build();
+    }
 }
